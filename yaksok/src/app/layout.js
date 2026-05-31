5da109c50b20da3c66616ae482af12d9d678f8ea
wkwjsrj07-gif <wkwjsrj07@gmail.com>
2026-05-31 15:36:12 +0900

    Add files via upload

---
+++ yaksok/src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: '약속잡기 캘린더',
+  description: '모두가 되는 날을 한눈에 찾는 약속잡기 도구',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
