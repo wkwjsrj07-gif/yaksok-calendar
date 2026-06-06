fc17f0886768fdc576e521d6521fd58a55593764
wkwjsrj07-gif <wkwjsrj07@gmail.com>
2026-06-06 16:32:11 +0900

    Add files via upload

---
+++ yaksok/src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://gaffxiaaeoitjzxyriyv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZmZ4aWFhZW9pdGp6eHlyaXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTA0MjAsImV4cCI6MjA5NTYyNjQyMH0.e3NFCZzipJA6-1Y-MjBK8gIBhQh2H4EW7vmtKO9J-pY'
+);
